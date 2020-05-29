a202661906303e746049e7eeca5ffa42d1306cfe
Luclu7 <me@luclu7.fr>
2020-05-29 14:44:10 +0200

    bbb

---
+++ buildPackage.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/strslice"
+	"github.com/docker/docker/client"
+	"io"
+	"os"
+)
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func buildPackage(packageName string) {
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	imageName := "docker.io/maximbaz/arch-build-aur"
+
+	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, out)
+
+	var command strslice.StrSlice
+	command = strslice.StrSlice(append([]string{"/bin/bash", "-c"}, "/build-aur "+packageName+""))
+
+	sourceFolder := getEnv("HOST_FOLDER", "/home/luclu7")
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: imageName,
+		Cmd:   command,
+	},
+		&container.HostConfig{
+			Mounts: []mount.Mount{
+				{
+					Type:   mount.TypeBind,
+					Source: sourceFolder,
+					Target: "/pkg",
+				},
+			},
+		},
+		nil,
+		"")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp.ID)
+}
