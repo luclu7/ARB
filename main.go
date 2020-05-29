968ba5024699fb571e96ce122cd9ad413b020b6a
Luclu7 <me@luclu7.fr>
2020-05-29 14:26:41 +0200

    aaa

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/luclu7/ARB/docker"
+	docker "github.com/luclu7/ARB/docker"
 	"net/http"
 )
 
@@ -12,13 +12,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerBuild(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(request)
+	pkg := vars["package"]
+	docker.buildPackage(pkg)
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler)
-	r.HandleFnunc("/build/{package}", handlerBuild)
+	r.HandleFunc("/build/{package}", handlerBuild)
 	http.Handle("/", r)
 	fmt.Println("Starting on localhost:8081...")
 	http.ListenAndServe(":8081", r)
