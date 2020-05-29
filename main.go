bc7180cbf9707e2187bc928a1c9d5754f5fe231e
Luclu7 <me@luclu7.fr>
2020-05-29 06:56:43 +0200

    aaa

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/luclu7/ARB/docker"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hey, it works !")
+}
+
+func handlerBuild(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	r.HandleFnunc("/build/{package}", handlerBuild)
+	http.Handle("/", r)
+	fmt.Println("Starting on localhost:8081...")
+	http.ListenAndServe(":8081", r)
+}
